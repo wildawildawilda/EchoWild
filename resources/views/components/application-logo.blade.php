5b2a4aa4154f711ae06c26c1f2b3e078b99e9673
wildawildawilda <wildawildawilda89@gmail.com>
2026-07-18 17:05:32 +0800

    logo and web name

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,1 @@
-<svg viewBox="0 0 200 50" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <text x="0" y="35" font-family="'Outfit', sans-serif" font-weight="700" font-size="32" fill="currentColor">
-        Echo<tspan class="text-emerald-500">Wild</tspan>
-    </text>
-</svg>
+<img src="{{ asset('images/logo.png') }}" alt="EchoWild Logo" {{ $attributes->merge(['class' => 'object-contain']) }}>
